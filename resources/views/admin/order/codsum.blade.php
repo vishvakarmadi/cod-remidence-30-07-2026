75851ab7613b154123d8f93262abcc5ccd499efe
vishvakarmadi <adityav2316672@gmail.com>
2026-07-31 11:21:44 +0530

    asfdgfhdsfgfhjasfxd

--- resources/views/admin/order/codsum.blade.php
+++ resources/views/admin/order/codsum.blade.php
@@ -80,7 +80,10 @@ use App\Models\Admin\Order;
                                     <script>
                                         $(document).ready(function() {
                                             $('#multiple-checkboxes').multiselect({
-                                            includeSelectAllOption: true,
+                                                includeSelectAllOption: true,
+                                                enableFiltering: true,
+                                                enableCaseInsensitiveFiltering: true,
+                                                filterPlaceholder: 'Search User / Seller...'
                                             });
                                         });
                                     </script>
